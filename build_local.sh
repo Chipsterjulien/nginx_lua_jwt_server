c80ecbe529307a4cbcc94056834635a73a9db57d
Chipster <julien.chipster@gmail.com>
2020-08-11 12:53:00 +0200

    Revert "Test if db/dbFile.db exists and if not, create it with adding easy login/password"
    This reverts commit e03422660f9a9ff4744197c4e0b04cb27fb447c9.

--- build_local.sh
+++ build_local.sh
@@ -1,8 +1,5 @@
 # Création du répertoire db en y plaçant la base de donnée dedans
-if [ ! -e "db/dbFile.db" ]; then
-    mkdir db && cp dbFile.db db
-    echo -e "\nadmin admin" >> db/dbFile.db
-fi
+mkdir db && cp *.db db
 # Création de l'image
 docker build -f Dockerfile_local -t img_testing_guialarm:v0.9 .
 # Création du container
