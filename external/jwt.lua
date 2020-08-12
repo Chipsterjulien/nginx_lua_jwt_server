8908577455a1efd3cec07ea5a1c25c5ed8d52bd6
Chipster <julien.chipster@gmail.com>
2020-08-12 09:40:05 +0200

    Replace all " by '

--- external/jwt.lua
+++ external/jwt.lua
@@ -1,6 +1,6 @@
-local basexx = require( "basexx" )
-local sha2 = require( "sha2" )
-local json = require( "json" )
+local basexx = require( 'basexx' )
+local sha2 = require( 'sha2' )
+local json = require( 'json' )
 local splitN = require( 'splitN' )
 
 local alg_sign = {
@@ -18,13 +18,13 @@ local alg_verify = {
 local jwt = {}
 
 function jwt.sign( payload, key, alg )
-  if type(payload) ~= 'table' then return nil, nil, "Argument #1 must be table" end
-  if type(key) ~= 'string' then return nil, nil, "Argument #2 must be string" end
+  if type(payload) ~= 'table' then return nil, nil, 'Argument #1 must be table' end
+  if type(key) ~= 'string' then return nil, nil, 'Argument #2 must be string' end
 
   alg = alg or 'HS256'
 
   if not alg_sign[alg] then
-    return nil, nil, "Algorithm not supported"
+    return nil, nil, 'Algorithm not supported'
   end
 
   local header = { typ = 'JWT', alg = alg }
@@ -42,7 +42,7 @@ function jwt.sign( payload, key, alg )
   }
 
   -- Concat headers and payload
-  local signing_input = table.concat(segments, ".")
+  local signing_input = table.concat(segments, '.')
   -- Sign with right algo and get hexa signature
   local signature_hex = alg_sign[alg](signing_input, key)
   -- Convert hexa signature to bin
@@ -55,7 +55,7 @@ end
 
 function jwt.refresh( JWTstr, key, validityPeriod )
   if validityPeriod == nil then validityPeriod = 3600 end
-  if type( validityPeriod ) ~= 'number' then return nil, "Argument #3 must be nil or a number" end
+  if type( validityPeriod ) ~= 'number' then return nil, 'Argument #3 must be nil or a number' end
 
   local body, header, err = jwt.verify( JWTstr, key, true )
   if err then
@@ -69,13 +69,13 @@ end
 
 function jwt.verify( JWTstr, key, verify )
   if key and verify == nil then verify = true end
-  if type( JWTstr ) ~= 'string' then return nil, "Argument #1 must be string" end
-  if verify and type( key ) ~= 'string' then return nil, "Argument #2 must be string" end
+  if type( JWTstr ) ~= 'string' then return nil, 'Argument #1 must be string' end
+  if verify and type( key ) ~= 'string' then return nil, 'Argument #2 must be string' end
 
   local token = splitN( JWTstr , '.')
 
   if #token ~= 3 then
-    return nil, "Invalid token"
+    return nil, 'Invalid token'
   end
 
   -- Can use unpack but there is somes issues with old lua version so, I prefer use as below
@@ -88,40 +88,40 @@ function jwt.verify( JWTstr, key, verify )
   end)
 
   if not ok then
-    return nil, nil, "Invalid json"
+    return nil, nil, 'Invalid json'
   end
 
   if verify then
-    if not header.typ or header.typ ~= "JWT" then
-      return nil, nil, "Invalid typ"
+    if not header.typ or header.typ ~= 'JWT' then
+      return nil, nil, 'Invalid typ'
     end
 
-    if not header.alg or type(header.alg) ~= "string" then
-      return nil, nil, "Invalid alg"
+    if not header.alg or type(header.alg) ~= 'string' then
+      return nil, nil, 'Invalid alg'
     end
 
-    if body.exp and type(body.exp) ~= "number" then
-      return nil, nil, "exp must be number"
+    if body.exp and type(body.exp) ~= 'number' then
+      return nil, nil, 'exp must be number'
     end
 
-    if body.nbf and type(body.nbf) ~= "number" then
-      return nil, nil, "nbf must be number"
+    if body.nbf and type(body.nbf) ~= 'number' then
+      return nil, nil, 'nbf must be number'
     end
 
     if not alg_verify[header.alg] then
-      return nil, nil, "Algorithm not supported"
+      return nil, nil, 'Algorithm not supported'
     end
 
     if not alg_verify[header.alg](headerb64 .. "." .. bodyb64, sig, key) then
-      return nil, nil, "Invalid signature"
+      return nil, nil, 'Invalid signature'
     end
 
     if body.exp and os.time() >= body.exp then
-      return nil, nil, "Not acceptable by exp"
+      return nil, nil, 'Not acceptable by exp'
     end
 
     if body.nbf and os.time() < body.nbf then
-      return nil, nil, "Not acceptable by nbf"
+      return nil, nil, 'Not acceptable by nbf'
     end
   end
 
