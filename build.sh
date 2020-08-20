a5d0a8120deca52e9ab03f574b558c801390b7b5
Chipster <julien.chipster@gmail.com>
2020-08-20 21:34:34 +0200

    Add omitted volume

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # Création de l'image
 docker build -t img_alpine_rpi_guialarm_server:v1.0.4 .
 # Création du container
-docker run -d --restart unless-stopped --name container_alpine_rpi_guialarm-v1.0.4 -p 8090:8090 img_alpine_rpi_guialarm_server:v1.0.4 .
+docker run -d --restart unless-stopped --name container_alpine_rpi_guialarm-v1.0.4 -p 8090:8090 -v dataAlarm:/app/run img_alpine_rpi_guialarm_server:v1.0.4 .
